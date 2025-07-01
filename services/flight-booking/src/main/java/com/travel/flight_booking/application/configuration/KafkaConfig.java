3c95d2cfca714417a47077fe83903ea71c9ef78d
Rafael Almeida <rafael.profeng@gmail.com>
2025-07-01 20:53:27 -0300

    feat: Add kafka to flight-booking service

---
+++ services/flight-booking/src/main/java/com/travel/flight_booking/application/configuration/KafkaConfig.java
@@ -0,0 +1,9 @@
+package com.travel.flight_booking.application.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+}
